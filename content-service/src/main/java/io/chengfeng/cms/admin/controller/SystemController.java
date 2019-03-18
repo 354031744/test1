7adf1ec3d88fc6784a4dd4037b531e3f8e32fe80
354031744 <354031744@qq.com>
2019-03-18 18:01:54 +0800

    112

--- content-service/src/main/java/io/chengfeng/cms/admin/controller/SystemController.java
+++ content-service/src/main/java/io/chengfeng/cms/admin/controller/SystemController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SystemController {
     @GetMapping("test")
     public String setFriendshiplink(){
-        return "≤‚ ‘"
+        return "test11"
     }
 }
